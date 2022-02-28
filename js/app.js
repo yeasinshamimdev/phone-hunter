3ed0e7616d8869c4db14df216f5007cf9a084e4a
Yeasin <yeasinshamim7@gmail.com>
2022-03-01 02:20:05 +0600

    modal set done

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ const wrongMsg = document.getElementById('wrong-value-error');
 const toggleSpinner = (displaySpinner) => {
     document.getElementById('spinner').style.display = displaySpinner;
 }
-const toggleSpinner2 = (spinnerOn, spinnerOff) => {
-    document.getElementById('spinner').classList.add(spinnerOn);
-    document.getElementById('spinner').classList.remove(spinnerOff);
+const toggleSpinner2 = (addClass, removeClass) => {
+    document.getElementById('spinner').classList.add(addClass);
+    document.getElementById('spinner').classList.remove(removeClass);
 }
 
 // Load data from Server
@@ -33,7 +33,7 @@ const loadData = () => {
     }
     inputField.value = '';
 }
-
+loadData();
 // Display Data in the Card 
 const displayData = (phones) => {
     console.log(phones);
@@ -52,11 +52,11 @@ const displayData = (phones) => {
             div.innerHTML = `
             <div class="card h-100 justify-content-center">
                 <img src="${phone.image}" class="card-img-top img-fluid img-radius h-auto w-75 mx-auto pt-1" alt="My Image">
-                <div class="card-body mx-auto">
-                    <h5 class="card-title">Name: ${phone.phone_name}</h5>
-                    <p class="card-text">${phone.slug}</p>
+                <div class="card-body mx-auto text-dark">
+                    <h4 class="card-title">Name: ${phone.phone_name}</h4>
+                    <p class="card-text">Brand: ${phone.brand}</p>
                 </div>
-                <button class="btn btn-primary w-50 mx-auto mb-3 p-2 rounded-pill">More details</button>
+                <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary w-50 mx-auto mb-3 p-2 rounded-pill" data-bs-toggle="modal" data-bs-target="#exampleModal">More details</button>
             </div>
             `;
             cardsDiv.appendChild(div);
@@ -64,4 +64,25 @@ const displayData = (phones) => {
         toggleSpinner('none');
         toggleSpinner2('d-none', 'd-flex');
     }
+}
+
+// Details information function 
+const loadDetails = (details) => {
+    console.log(details);
+    fetch(`https://openapi.programming-hero.com/api/phone/${details}`)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data));
+}
+
+// Display details function
+const displayDetails = (detail) => {
+    console.log(detail);
+    const modalImg = document.getElementById('modal-img');
+    modalImg.setAttribute('src', `${detail.image}`);
+    const modalDetails = document.getElementById('modal-details');
+    modalDetails.classList.add('mt-3');
+    modalDetails.innerHTML = `
+        <h4><b>Name:</b> ${detail.name}</h4>
+        <h6><b>Release:</b>  ${detail.releaseDate ? detail.releaseDate : 'Release Date not available'} </h6>
+    `;
 }
